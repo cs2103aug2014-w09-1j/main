afa3f276a8df38e4ae2ea0b01555c336843fb4af
shuansiang <shuansiang@hotmail.com>
2014-09-22 12:12:57 +0800

    first edits to MyTasksStorage.java

--- src/mytasks/file/MyTasksStorage.java
+++ src/mytasks/file/MyTasksStorage.java
@@ -1,34 +1,76 @@
 package mytasks.file;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * MyTasksStorage handles the storage of tasks into external memory as well as converting it to readable
+ * MyTasksStorage handles the storage of tasks into external memory as well as
+ * converting it to readable
  * local memory for logical processes
- * @author Wilson
+ * 
+ * @author Tay Shuan Siang
  *
  */
-public class MyTasksStorage implements IStorage{
-	
-	//Constructor
-	public MyTasksStorage(){
-		
+public class MyTasksStorage implements IStorage {
+
+	private String externalMemoryFile = "externalmemoryfile.txt";
+	private ArrayList<String> localMemory = new ArrayList<String>();
+
+	// Constructor
+	public MyTasksStorage() {
+
 	}
 
 	/**
 	 * {@inheritDoc}
+	 * 
+	 * @throws IOException
 	 */
 	public ArrayList<String> readExtMem(String fileName) {
-		// TODO Auto-generated method stub
-		return null;
+		File f = new File(externalMemoryFile);
+		if (!f.exists()) {
+			writeExtMem(localMemory);
+		}
+
+		String line = null;
+		// BufferedReader bufferedReader;
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(
+							externalMemoryFile));
+			while ((line = bufferedReader.readLine()) != null) {
+				localMemory.add(line);
+			}
+			bufferedReader.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return localMemory;
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public void writeExtMem(ArrayList<String> localMem) {
-		// TODO Auto-generated method stub
-		
+	public void writeExtMem(ArrayList<String> localMemory) {
+		try {
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(externalMemoryFile));
+			for (int i=0; i<localMemory.size(); i++) {
+				bufferedWriter.write(localMemory.get(i));
+				bufferedWriter.newLine();
+			}
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 }
