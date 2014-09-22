72a495a0c95ec3b929a7f31546c986a64e9d71e3
huiwenhw <huiwen_28@hotmail.com>
2014-09-22 12:27:37 +0800

    Revert "first edits to MyTasksStorage.java"
    This reverts commit afa3f276a8df38e4ae2ea0b01555c336843fb4af.

--- src/mytasks/file/MyTasksStorage.java
+++ src/mytasks/file/MyTasksStorage.java
@@ -1,76 +1,34 @@
 package mytasks.file;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * MyTasksStorage handles the storage of tasks into external memory as well as
- * converting it to readable
+ * MyTasksStorage handles the storage of tasks into external memory as well as converting it to readable
  * local memory for logical processes
- * 
- * @author Tay Shuan Siang
+ * @author Wilson
  *
  */
-public class MyTasksStorage implements IStorage {
-
-	private String externalMemoryFile = "externalmemoryfile.txt";
-	private ArrayList<String> localMemory = new ArrayList<String>();
-
-	// Constructor
-	public MyTasksStorage() {
-
+public class MyTasksStorage implements IStorage{
+	
+	//Constructor
+	public MyTasksStorage(){
+		
 	}
 
 	/**
 	 * {@inheritDoc}
-	 * 
-	 * @throws IOException
 	 */
 	public ArrayList<String> readExtMem(String fileName) {
-		File f = new File(externalMemoryFile);
-		if (!f.exists()) {
-			writeExtMem(localMemory);
-		}
-
-		String line = null;
-		// BufferedReader bufferedReader;
-		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader(
-							externalMemoryFile));
-			while ((line = bufferedReader.readLine()) != null) {
-				localMemory.add(line);
-			}
-			bufferedReader.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return localMemory;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public void writeExtMem(ArrayList<String> localMemory) {
-		try {
-			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(externalMemoryFile));
-			for (int i=0; i<localMemory.size(); i++) {
-				bufferedWriter.write(localMemory.get(i));
-				bufferedWriter.newLine();
-			}
-			bufferedWriter.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void writeExtMem(ArrayList<String> localMem) {
+		// TODO Auto-generated method stub
+		
 	}
-
+	
 }
