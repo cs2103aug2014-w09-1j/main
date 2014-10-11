e9e6b87f53cd7aa09c3487dd9bd7719e3f574e0b
Michael <khlee1886@gmail.com>
2014-10-11 22:06:21 +0800

    Failed test 3 for getSnapshot(). Unable to handle task with two dates(from ... to ...).

--- src/mytasks/logic/LogicTest.java
+++ src/mytasks/logic/LogicTest.java
@@ -104,6 +104,16 @@ public class LogicTest {
 			     + "CS2101 meeting on 29.09.2014\n"
 			     + "medical check up on 01.10.2014 13:00\n"
 			     + "play badminton\n", taskLogic.obtainPrintableOutput());
+		// sort by date - test 3
+		taskLogic.executeCommand("add pay acceptance fee from 28.09.2014 to 8.10.2014");
+		taskLogic.executeCommand("add do PS4 from 28.09.2014 to 5.10.2014");
+		assertEquals("CS2103T meeting on 22.09.2014 #important\n"
+			     + "CS2100 Midterm on 25.09.2014\n"
+				 + "do PS4 from 28.09.2014 to 05.10.2014\n"
+			     + "pay acceptance fee from 28.09.2014 to 08.10.2014\n"
+			     + "CS2101 meeting on 29.09.2014\n"
+			     + "medical check up on 01.10.2014 13:00\n"
+			     + "play badminton\n", taskLogic.obtainPrintableOutput());
 	}
 	
 	
