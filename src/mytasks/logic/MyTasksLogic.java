1791155c1bd4eb3babd5fc0e8f640aabbf2fa512
shuansiang <shuansiang@hotmail.com>
2014-10-19 01:34:25 +0800

    implemented undo for add and delete
    still working on undo for update, junit testing for all 3 and redo

--- src/mytasks/logic/MyTasksLogic.java
+++ src/mytasks/logic/MyTasksLogic.java
@@ -1,19 +1,22 @@
 package mytasks.logic;
 
+import mytasks.file.Task;
 import mytasks.parser.IParser;
 import mytasks.parser.MyTasksParser;
 import mytasks.storage.IStorage;
 import mytasks.storage.MyTasksStorage;
 
 /**
- * MyTasksLogic handles all logic related operations such as program flow and execution of commands
+ * MyTasksLogic handles all logic related operations such as program flow and
+ * execution of commands
+ * 
  * @author Wilson, Huiwen, Michael
  *
  */
 
-public class MyTasksLogic implements ILogic{
-	
-	//Private variables
+public class MyTasksLogic implements ILogic {
+
+	// Private variables
 	private IParser mParser;
 	private IStorage mStorage;
 	private LocalMemory mLocalMem;
@@ -21,77 +24,116 @@ public class MyTasksLogic implements ILogic{
 	private boolean isDeveloper;
 	private static String MESSAGE_SEARCH_FAIL = "unable to find task with keyword '%1$s'";
 	private static String MESSAGE_SEARCH_SUCCESS = "task(s) with keyword '%1$s' searched";
-	
-	//TODO: introduce get Instance to all classes. This included. Read notes/google this
-	//Difference between creating a public getInstance for checking yourself, 
-	//and getinstance for your member variables 
-	//Constructor
-	public MyTasksLogic(boolean isDeveloper){
+
+	// TODO: introduce get Instance to all classes. This included. Read
+	// notes/google this
+	// Difference between creating a public getInstance for checking yourself,
+	// and getinstance for your member variables
+	// Constructor
+	public MyTasksLogic(boolean isDeveloper) {
 		initLogic(isDeveloper);
 	}
 
 	/**
-	 * initProgram initializes all local variables to prevent and data overflow from previous sessions
+	 * initProgram initializes all local variables to prevent and data overflow
+	 * from previous sessions
 	 */
-	private void initLogic(boolean isDev){
+	private void initLogic(boolean isDev) {
 		isDeveloper = isDev;
 		mParser = new MyTasksParser();
 		mStorage = MyTasksStorage.getInstance();
 		mLocalMem = LocalMemory.getInstance();
 		if (!isDeveloper) {
-			mLocalMem.loadLocalMemory();	
+			mLocalMem.loadLocalMemory();
 		}
 		mViewHandler = new MemorySnapshotHandler();
 	}
-	
+
 	/**
 	 * {@inheritDoc}
-	 */	
+	 */
 	public String executeCommand(String input) {
-		
+
 		Command commandObject = parseInput(input);
 		String output = removeFirstWord(input);
-		
-		switch(commandObject.getType()) {
-			case ADD:
+
+		switch (commandObject.getType()) {
+			case ADD :
 				addCommand(commandObject);
-				if(!isDeveloper){
-					mLocalMem.saveLocalMemory(); 
+				putToUndoStack(commandObject);
+				if (!isDeveloper) {
+					mLocalMem.saveLocalMemory();
 				}
 				return output + " added";
-			case DELETE:
+			case DELETE :
+				putToUndoStack(commandObject);
 				deleteCommand(commandObject);
-				if(!isDeveloper){
-					mLocalMem.saveLocalMemory(); 
-				}				
+				if (!isDeveloper) {
+					mLocalMem.saveLocalMemory();
+				}
 				return output + " deleted";
-			case UPDATE:
+			case UPDATE :
 				updateCommand(commandObject);
-				if(!isDeveloper){
-					mLocalMem.saveLocalMemory(); 
-				}				
-				output = input.replace(input.trim().split("[-]+")[0], "").trim();	
-				output = output.replace(output.trim().split("\\s+")[0], "").trim();	
-				//mLocalMem.print();
+				putToUndoStack(commandObject);
+				if (!isDeveloper) {
+					mLocalMem.saveLocalMemory();
+				}
+				output = input.replace(input.trim().split("[-]+")[0], "")
+								.trim();
+				output = output.replace(output.trim().split("\\s+")[0], "")
+								.trim();
+				// mLocalMem.print();
 				return output + " updated";
-			case SORT:
+			case SORT :
 				sortCommand(commandObject);
 				return output + " sorted";
-			case SEARCH:
+			case SEARCH :
 				boolean isFound = searchCommand(commandObject);
-				if (isFound){
+				if (isFound) {
 					return String.format(MESSAGE_SEARCH_SUCCESS, output);
-				} else{
+				} else {
 					return String.format(MESSAGE_SEARCH_FAIL, output);
 				}
-			case UNDO:
+			case UNDO :
 				undoCommand();
+				if (!isDeveloper) {
+					mLocalMem.saveLocalMemory();
+				}
 				return "";
-			case REDO:
+			case REDO :
 				redoCommand();
+				if (!isDeveloper) {
+					mLocalMem.saveLocalMemory();
+				}
 				return "";
 			default:
 				return "invalid command";
+		}
+	}
+
+	private void putToUndoStack(Command commandObject) {
+		// TODO Auto-generated method stub
+		Command commandToUndo;
+		switch (commandObject.getType()) {
+			case ADD :
+				commandToUndo = new DeleteCommand("delete", commandObject.getTask().getDescription(), null, null, null, null);
+				mLocalMem.push(commandToUndo);
+				return;
+			case DELETE :
+				for (int i=0; i<mLocalMem.getLocalMem().size(); i++) {
+					if (mLocalMem.getLocalMem().get(i).getDescription().equals(commandObject.getTask().getDescription())) {
+						Task tempTask = mLocalMem.getLocalMem().get(i);
+						commandToUndo = new AddCommand("add", commandObject.getTask().getDescription(), tempTask.getFromDateTime(), tempTask.getToDateTime(), tempTask.getLabels(), null);
+						mLocalMem.push(commandToUndo);
+						break;
+					}
+				}
+				return;
+			case UPDATE :
+				
+				return;
+			default:
+				return;
 		}
 	}
 
@@ -99,7 +141,7 @@ public class MyTasksLogic implements ILogic{
 		int i = input.indexOf(' ');
 		return input.substring(i).trim();
 	}
-	
+
 	private void addCommand(Command commandObject) {
 		mLocalMem.add(commandObject.getTask());
 	}
@@ -109,7 +151,8 @@ public class MyTasksLogic implements ILogic{
 	}
 
 	private void updateCommand(Command commandObject) {
-		mLocalMem.update(commandObject.getToUpdateTaskDesc(), commandObject.getTask());
+		mLocalMem.update(commandObject.getToUpdateTaskDesc(),
+						commandObject.getTask());
 	}
 
 	private void sortCommand(Command commandObject) {
@@ -117,20 +160,23 @@ public class MyTasksLogic implements ILogic{
 	}
 
 	private boolean searchCommand(Command commandObject) {
-		return mLocalMem.search(commandObject.getTask());				
+		return mLocalMem.search(commandObject.getTask());
 	}
-	
+
 	private void undoCommand() {
 		// TODO Auto-generated method stub
-		
+		mLocalMem.undo();
 	}
-	
+
 	private void redoCommand() {
 		// TODO Auto-generated method stub
+		mLocalMem.redo();
 
 	}
+
 	/**
-	 * parseInput calls the parser to read and understand user input 
+	 * parseInput calls the parser to read and understand user input
+	 * 
 	 * @param userInput
 	 * @return CommandType object that contains the relevant fields
 	 */
@@ -138,15 +184,15 @@ public class MyTasksLogic implements ILogic{
 		Command input = mParser.parseInput(userInput);
 		return input;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	public String obtainPrintableOutput() {
 		return mViewHandler.getSnapshot(mLocalMem);
 	}
-	
-	protected LocalMemory getMemory(){
+
+	protected LocalMemory getMemory() {
 		return mLocalMem;
 	}
 }
