2f2b62c048d61d79aaecf839a9c11248ef6b1a46
huiwenhw <huiwen_28@hotmail.com>
2014-11-07 18:59:23 +0800

    Implemented hide command for GUI
    Added more tests for integration testing

--- src/mytasks/logic/HelpCommand.java
+++ src/mytasks/logic/HelpCommand.java
@@ -47,8 +47,7 @@ public class HelpCommand extends Command {
 		frame.setVisible(true);
 
 		// pressing the esc key will close the window
-		frame.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
-						.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "EXIT");
+		frame.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "EXIT");
 		frame.getRootPane().getActionMap().put("EXIT", new AbstractAction() {
 			public void actionPerformed(ActionEvent e) {
 				frame.dispose();
